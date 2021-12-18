64f4d20d6329ec9cb6f38f944dcb6366ec17e3b5
Jerzy Sykut <thejurass@gmail.com>
2021-12-18 20:57:28 +0100

    NiezrozumiałÄe

---
+++ zrozumiec.py
@@ -0,0 +1,45 @@
+#message = "Hello "
+#imie = "Jurek "
+
+#print(message + imie )
+#message = "Kalkulator Kredytowy"
+#print(message)
+#print( "Podaj imie")
+#imie = input()
+
+#wiadomosc = "Czesc"
+#print(wiadomosc + imie)
+#print("kredytu Konsumencki")
+#print("wielkosc kredytu")
+#wiekosc kredytu = float(input())
+#wielkosc kredytu = float(input())
+#inflacja = Styczeń	1,59282448436825
+
+#=(1 + ((inflacja+oprocentowanie)/1200)) * kwota kredytu - kwota raty
+#sty1 = 	1.59282448436825
+#print("Kredyt konsumencki")
+##print(" wysokosckredytu")
+#wysokosckredytu = loat(input())
+#print(oprocentowanie)
+#oprocentowanie = float(input())
+#print("Na koniec podaj kwote raty")
+#rata = int(input())
+#wzor = (1+(sty1+oprocentowanie)1200)*wysokosc kredytu - rata
+#roznica = (wysokosc kredytu-wzor)
+
+
+print("KredyKonsumencki")
+print("kwotakredytu")
+kwotakredytu = int(input())
+print("procentkredytu")
+oprocentowanie = float(input())
+print("wysokoscraty")
+wysokoscraty = float(input())
+infStyc1 = 1,59282448436825
+zostalo = ("Pozostała kwota")
+mniej = ("Mniej niz poprzednim miesiacu")
+wzor = (1 + ((infStyc1 + oprocentowanie )/1200)) * kwotakredytu - wysokoscraty
+print("wzor")
+
+
+
